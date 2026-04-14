eac43748b9ad38af769b7502731ab120f5505792
PudgyPigeon <tommynam1994@gmail.com>
2026-04-14 20:01:14 +0000

    Fix: MCP not configured

--- apps/kubernetes-mcp/src/Kubernetes.hs
+++ apps/kubernetes-mcp/src/Kubernetes.hs
@@ -15,14 +15,16 @@ import qualified Data.Text.Encoding as TE
 import qualified Data.ByteString.Lazy as BSL
 import Data.Aeson (encode, ToJSON)
 import Kubernetes.Client (KubeConfigSource(..), mkKubeClientConfig)
-import Kubernetes.OpenAPI (Accept(..), MimeJSON(..), dispatchMime, MimeResult(..), KubernetesRequest)
+import Kubernetes.OpenAPI (Accept(..), MimeJSON(..), MimePlainText(..), dispatchMime, MimeResult(..), KubernetesRequest)
+import Kubernetes.OpenAPI.MimeTypes (Produces, MimeUnrender, MimeType)
+import Kubernetes.OpenAPI.Model (Namespace(..), Name(..))
 import qualified Kubernetes.OpenAPI.API.CoreV1 as CoreV1
 import qualified Kubernetes.OpenAPI.API.AppsV1 as AppsV1
 import System.Environment (getEnv)
 import Types (ResourceType(..))
 
 -- | Boilerplate abstractor for executing an arbitrary typed SDK request
-runApiRequest :: (ToJSON res) => KubernetesRequest req contentType res accepts -> IO (Either Text Text)
+runApiRequest :: (ToJSON res, Produces req accepts, MimeUnrender accepts res, MimeType contentType) => KubernetesRequest req contentType res accepts -> IO (Either Text Text)
 runApiRequest req = do
     home <- getEnv "HOME"
     oidcCache <- atomically $ newTVar Map.empty
@@ -30,16 +32,34 @@ runApiRequest req = do
     
     res <- try (dispatchMime mgr kcfg req)
     case res of
-        Right (MimeResult _ _ body) -> 
+        Right (MimeResult (Right body) _) -> 
             return . Right . TE.decodeUtf8 . BSL.toStrict $ encode body
+        Right (MimeResult (Left mimeErr) _) -> 
+            return . Left . T.pack $ show mimeErr
+        Left err -> 
+            return . Left . T.pack $ show (err :: SomeException)
+
+-- | Special handler for logs returning flat Text
+runTextRequest :: (Produces req accepts, MimeUnrender accepts Text, MimeType contentType) => KubernetesRequest req contentType Text accepts -> IO (Either Text Text)
+runTextRequest req = do
+    home <- getEnv "HOME"
+    oidcCache <- atomically $ newTVar Map.empty
+    (mgr, kcfg) <- mkKubeClientConfig oidcCache (KubeConfigFile (home ++ "/.kube/config"))
+    
+    res <- try (dispatchMime mgr kcfg req)
+    case res of
+        Right (MimeResult (Right body) _) -> 
+            return $ Right body
+        Right (MimeResult (Left mimeErr) _) -> 
+            return . Left . T.pack $ show mimeErr
         Left err -> 
             return . Left . T.pack $ show (err :: SomeException)
 
 -- | Pure function bridging the ADT routing logic
 resourceRequest :: ResourceType -> Text -> IO (Either Text Text)
-resourceRequest Pods ns = runApiRequest $ CoreV1.listNamespacedPod (Accept MimeJSON) ns
-resourceRequest Services ns = runApiRequest $ CoreV1.listNamespacedService (Accept MimeJSON) ns
-resourceRequest Deployments ns = runApiRequest $ AppsV1.listNamespacedDeployment (Accept MimeJSON) ns
+resourceRequest Pods ns = runApiRequest $ CoreV1.listNamespacedPod (Accept MimeJSON) (Namespace ns)
+resourceRequest Services ns = runApiRequest $ CoreV1.listNamespacedService (Accept MimeJSON) (Namespace ns)
+resourceRequest Deployments ns = runApiRequest $ AppsV1.listNamespacedDeployment (Accept MimeJSON) (Namespace ns)
 
 -- | Public Facing API endpoints
 getNamespaces :: IO (Either Text Text)
@@ -49,5 +69,5 @@ getResources :: ResourceType -> Text -> IO (Either Text Text)
 getResources = resourceRequest
 
 getLogs :: ResourceType -> Text -> Text -> IO (Either Text Text)
-getLogs Pods name ns = runApiRequest $ CoreV1.readNamespacedPodLog name ns
+getLogs Pods name ns = runTextRequest $ CoreV1.readNamespacedPodLog (Accept MimePlainText) (Name name) (Namespace ns)
 getLogs _ _ _ = return $ Left "Fetching logs is currently only supported for Pods."
