e9ff10cf110b93087c099a977541c68c0aaff551
PudgyPigeon <tommynam1994@gmail.com>
2026-05-16 15:47:20 +0000

    style: Run erlfmt

--- apps/don-erleone/test/de_utils_tests.erl
+++ apps/don-erleone/test/de_utils_tests.erl
@@ -2,22 +2,23 @@
 %% Copyright (C) 2026 Tommy (Thae Hyun) Nam <tommynam1994@gmail.com>
 
 -module(de_utils_tests).
+
 -include_lib("eunit/include/eunit.hrl").
 
 to_list_binary_test() ->
-  ?assertEqual("hello", de_utils:to_list(<<"hello">>)).
+    ?assertEqual("hello", (de_utils:to_list(<<"hello">>))).
 
 to_list_list_test() ->
-  ?assertEqual("world", de_utils:to_list("world")).
+    ?assertEqual("world", (de_utils:to_list("world"))).
 
 to_list_other_test() ->
-  ?assertEqual("123", de_utils:to_list(123)).
+    ?assertEqual("123", (de_utils:to_list(123))).
 
 any_to_int_list_test() ->
-  ?assertEqual(123, de_utils:any_to_int("123")).
+    ?assertEqual(123, (de_utils:any_to_int("123"))).
 
 any_to_int_binary_test() ->
-  ?assertEqual(456, de_utils:any_to_int(<<"456">>)).
+    ?assertEqual(456, (de_utils:any_to_int(<<"456">>))).
 
 any_to_int_atom_test() ->
-  ?assertEqual(789, de_utils:any_to_int('789')).
+    ?assertEqual(789, (de_utils:any_to_int('789'))).
